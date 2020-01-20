7ade3ffd74883b56f5c244079b29cac744ccb7d9
jakobleon <jakob.leon1@gmx.at>
2020-01-20 13:12:14 +0100

    Location anzeigen

--- app/src/main/java/com/example/trackingsystem/Activity1.java
+++ app/src/main/java/com/example/trackingsystem/Activity1.java
@@ -5,6 +5,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,8 +14,14 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Activity1 extends AppCompatActivity implements LocationListener {
 private static LocationManager locMan = null;
+public static int x = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,8 +56,27 @@ private static LocationManager locMan = null;
     private void displayLocation(Location location) {
         TextView lo = findViewById(R.id.cd1);
         TextView la = findViewById(R.id.cd2);
+        TextView datetime = findViewById(R.id.datetime);
 
-        double lat =
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        lo.setText(String.format("%.4f", lon));
+        la.setText(String.format("%.4f", lat));
+
+        Calendar cal = Calendar.getInstance();
+        Date today = cal.getTime();
+        SimpleDateFormat sdfD = new SimpleDateFormat("dd/MM/YYYY");
+        String date = sdfD.format(today);
+        SimpleDateFormat sdft = new SimpleDateFormat("HH:mm:ss:SSS");
+        String time = sdft.format(today);
+
+        datetime.setText(date + "     " + time);
+
+        //MySQLiteHelper dbhelper = new MySQLiteHelper(this);
+        //SQLiteDatabase db = dbhelper.getReadableDatabase();
+        //db.execSQL("INSERT INTO Data " +
+          //      "VALUES(x+1, lon, lat, date, time)");
     }
 
     @Override
